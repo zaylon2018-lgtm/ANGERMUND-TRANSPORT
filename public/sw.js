6a4dfe1794c12355f61d465f7b9933e8c6f57638
zaylon2018-lgtm <zaylon2018@gmail.com>
2026-05-18 02:12:47 +0200

    Delete public directory

--- public/sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>e.waitUntil(caches.open('angermund-full-v1').then(c=>c.addAll(['index.html','styles.css','app.js','manifest.json','truck-bg.jpg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
